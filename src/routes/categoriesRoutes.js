a1a4f7adf7f039260cbb90bcf6b5395229eedcab
LocalHostDiluk <169072534+LocalHostDiluk@users.noreply.github.com>
2025-04-10 08:24:37 -0600

    Update categoriesRoutes.js

--- src/routes/categoriesRoutes.js
+++ src/routes/categoriesRoutes.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { getCategories, createCategory, deleteCategory } from "../controllers/categoriesController.js";
+import { getCategories, createCategory, deleteCategory, updateCategory  } from "../controllers/categoriesController.js";
 
 const router = express.Router();
 
 router.get("/", getCategories);
 router.post("/create", createCategory);
-//router.put("/:id", updateProduct);
+router.patch("/update/:id", updateCategory);
 router.delete("/:id", deleteCategory);
 
 export default router;
